6f52e7663f3c6558560d11af1e655898e4fce994
Thais <thaisachmidtdasilva@gmail.com>
2024-02-08 20:13:08 -0300

    projeto finalizado - faltou os botoes trocarem de cor - fico devendo

--- INDMO/atividades/atvINDMO02/atv1/src/pages/Home/index.js
+++ INDMO/atividades/atvINDMO02/atv1/src/pages/Home/index.js
@@ -1,30 +1,63 @@
-import { SafeAreaView, Text, StyleSheet } from 'react-native';
+import { SafeAreaView, Text, StyleSheet, Image, View, TextInput, ScrollView, Platform } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
+
+const logoSenai = require('../../assets/logo-senai.png');
 
 export default function Home() {
 
     const navigation = useNavigation();
-    
+
     return (
 
-        <SafeAreaView  style={styles.container}>
+        <SafeAreaView style={styles.androidSafeArea}>
 
-            <Text>Thaís Schmidt da Silva</Text>
-            <Text>Sumaré, 07/02/2024</Text>
-            <Text>Escola SENAI Celso Charuri</Text>
-            <Text>Técnico em Desenvolvimento de Sistemas</Text>
-            <Text>INDMO - Interface para Dispositivos Móveis</Text>
-        
+            <ScrollView>
+
+                <View style={styles.container}>
+
+                    <View style={styles.box}>
+
+                        <Image
+                            source={logoSenai} style={styles.img}
+                        />
+
+                        <Text style={{ textAlign: 'center', fontSize: 20, fontWeight: 'bold' }}>Escola SENAI Celso Charuri</Text>
+
+                        <Text style={{ textAlign: 'center', fontSize: 17, fontWeight: 'bold', paddingTop: 5 }}>Técnico em Desenvolvimento de Sistemas</Text>
+
+                        <Text style={{ textAlign: 'center', fontSize: 14, fontWeight: 'bold', paddingTop: 5 }}>INDMO - Interface para Dispositivos Móveis</Text>
+
+                        <Text style={{ textAlign: 'center', fontSize: 17, fontWeight: 'bold', paddingTop: 70 }}>Thaís Schmidt da Silva</Text>
+
+                    </View>
+
+                    <Text style={{ textAlign: 'center', fontSize: 17, fontWeight: 'bold' , paddingTop: 340}}>Sumaré, 07/02/2024</Text>
+
+                </View>
+
+            </ScrollView>
+
         </SafeAreaView>
 
     )
 }
 
 const styles = StyleSheet.create({
+    androidSafeArea: {
+        paddingTop: Platform.OS === 'android' ? 35 : 0,
+    },
     container: {
         flex: 1,
-        backgroundColor: '#c1c1c11',
+        backgroundColor: '#dddede',
         alignItems: 'center',
         justifyContent: 'center',
+        width: '100%',
+        paddingBottom: 27,
+        paddingTop: 200
+    }, 
+    img: {
+        height: 90,
+        width: 300,
+        marginLeft: 16
     },
 });
